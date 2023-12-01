75b87e9ceffd68f989466373c48a7f04bb1470c6
aulanbekuulu <aulanbekuulu@srs.lan>
2023-12-01 16:29:39 +0600

    fix

---
+++ FactoryMethod/Program.cs
@@ -0,0 +1,19 @@
+﻿    using System;
+
+namespace FactoryMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            // new Client().Main();
+            // Console.WriteLine("App: Launched with the ConcreteCreator1.");
+            new Client().ClientCode(ConcreteCreator1.Invoke());
+            new Client().ClientCode(ConcreteCreator2.Invoke());
+        }
+
+        public static readonly Func<Creator> ConcreteCreator1 = () => new ConcreteCreator1();
+        public static readonly Func<Creator> ConcreteCreator2 = () => new ConcreteCreator2();
+    }
+}
