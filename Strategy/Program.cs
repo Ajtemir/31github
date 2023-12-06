a701cc90e9ff955a6fe9aa4d42d8bc905ea166d7
Aytemir <74861372+Ajtemir@users.noreply.github.com>
2023-12-06 09:04:53 +0600

    Update Program.cs

--- Strategy/Program.cs
+++ Strategy/Program.cs
@@ -47,6 +47,7 @@ namespace Strategy
     // The Strategy interface declares operations common to all supported
     // versions of some algorithm.
     //
+    
     // The Context uses this interface to call the algorithm defined by Concrete
     // Strategies.
     public interface IStrategy
