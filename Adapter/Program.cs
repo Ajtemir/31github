17158c74ea8c4157087b184e07a6b6da11b39852
Aytemir <74861372+Ajtemir@users.noreply.github.com>
2023-12-08 09:53:49 +0600

    Update Program.cs

--- Adapter/Program.cs
+++ Adapter/Program.cs
@@ -42,7 +42,6 @@ namespace Adapter
         {
             Adaptee adaptee = new Adaptee();
             ITarget target = new Adapter(adaptee);
-
             Console.WriteLine("Adaptee interface is incompatible with the client.");
             Console.WriteLine("But with adapter client can call it's method.");
 
